0dc918db355c6db48d8063494b7f4c704e4e128f
Jordan Walke <jordojw@gmail.com>
2014-03-09 21:44:44 -0700

    Updating to newer react-tools which has separate npm module now.
    Summary: Also, we no longer need react now.
    
    Test Plan:
    
    Reviewers:
    
    CC:

--- ftplugin/javascript/jslint/JSXTransformer/runner.js
+++ ftplugin/javascript/jslint/JSXTransformer/runner.js
@@ -3,17 +3,8 @@
 // runner for class desugar tranformation
 // check classes.js for more info
 
-
 var transform = require('jstransform').transform;
-var reactVisitors = require('react-tools/vendor/fbtransform/visitors').getVisitorsList();
-var es6Classes = require('jstransform/visitors/es6-class-visitors').visitorList;
-var es6RestParameters = require('jstransform/visitors/es6-rest-param-visitors').visitorList;
-var es6ArrowFunctions = require('jstransform/visitors/es6-arrow-function-visitors').visitorList;
-var es6ObjectShortNotation = require('jstransform/visitors/es6-object-short-notation-visitors').visitorList;
-
-var visitorList = reactVisitors.concat(
-  es6Classes, es6RestParameters, es6ArrowFunctions, es6ObjectShortNotation
-);
+var reactVisitors = require('react-tools/vendor/fbtransform/visitors').getAllVisitors();
 
 var source = '';
 process.stdin.resume();
@@ -22,6 +13,6 @@ process.stdin.on('data', function (chunk) {
   source += chunk;
 });
 process.stdin.on('end', function () {
-  var transformResult = transform(visitorList, source).code;
+  var transformResult = transform(reactVisitors, source).code;
   process.stdout.write(transformResult);
 });
